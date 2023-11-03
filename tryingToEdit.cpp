2388bdb4e5749a0637e5b572f7b1e17fc88c69d1
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2023-11-03 22:08:15 +0200

    Update tryingToEdit.cpp

--- tryingToEdit.cpp
+++ tryingToEdit.cpp
@@ -1,1 +1,11 @@
-//hey AGAIN
+/*editing from Visual studio 
+1 make your changes
+2 commit and add comments
+press "push"
+and the edits shall be viewed on the GitHub respsitory
+*/
+/* editing from GitHub
+1 make your changes
+2 commit them
+3 open Visual Studio and press "Pull"
+*/
